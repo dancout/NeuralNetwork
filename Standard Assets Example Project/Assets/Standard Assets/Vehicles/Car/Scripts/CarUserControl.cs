35158b323a64d3d8a3d516453718c64ccc38c5c5
Daniel Couturier <dancout@umich.edu>
2017-12-12 18:58:08 -0500

    Uploading working project of Units Standard Assets Example Project
    I made only slight edits to teh Car User Controller page. I made most of the floats public, so that they can be viewable in teh inpsector element when running the Car scene included in the unity standard assets example project.  Note the test where "input node" must be greater than 10 in order for the car to move.

---
+++ Standard Assets Example Project/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+
+    public float nodeInput;
+    public float h;
+    public float v;
+    public float handbrake;
+    public float mySteering;
+        public CarController m_Car; // the car controller we want to use
+
+
+        public void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        h = 0;
+        }
+
+
+        public void FixedUpdate()
+        {
+            // pass the input to the car!
+            
+        
+        h = CrossPlatformInputManager.GetAxis("Horizontal");
+        v = CrossPlatformInputManager.GetAxis("Vertical");
+#if !MOBILE_INPUT
+        handbrake = CrossPlatformInputManager.GetAxis("Jump");
+        if(nodeInput > 0.5)
+            m_Car.Move(h, v, v, handbrake);
+        else {
+            h = 0;
+            v = 0;
+            handbrake = 0;
+            m_Car.Move(h, v, v, handbrake); }
+
+        
+    
+#else
+
+#endif
+        }
+    }
+}
